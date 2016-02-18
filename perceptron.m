fa7d1efac3bc9a3ef9d6e6932588608c3a8b588d
dhingratul <atul.dhingra@rutgers.edu>
2016-02-18 17:40:56 -0500

    CS-520 version

--- perceptron.m
+++ perceptron.m
@@ -7,3 +7,44 @@ fea=full(fea);
 % [label, fea]=libsvmread('webspam_wc_normalized_unigram.svm');
 %%
 [train, test, labeltr, labelte]=datapartition(fea, label, 250000);
+    
+tic;
+w=zeros(2,size(train,2)); % Each feature has a weight
+b=0;
+wb=1;
+correct=0;
+ctr=1;
+for i=1:size(train,1)%100
+    for j=1:(size(w,1))
+%% Learning
+    z(j,1)=w(j,:)*train(i,:)';
+    end
+    [maximum, index] = max(z(:)); %Calculating argmax
+%% Weight Updates
+    if index~=labeltr(i,1)+1
+    w(labeltr(i,1)+1,:)=w(labeltr(i,1)+1,:)+train(i,:);
+    w(index,:)=w(index,:)-train(i,:);
+    end
+  
+% end
+% w=(w-min(w(:)))/(max(w(:))-min(w(:))); %Normalization of weight vector
+% save Weights w
+end
+ 
+time(ctr,1)=toc;
+
+time(ctr,1)=toc;
+%% Classification
+for i=1:size(test,1)%100
+    for j=1:(size(w,1))
+
+   z(j,1)=w(j,:)*test(i,:)';
+    end
+   [maximum, index] = max(z(:)); %Calculating argmax
+    if index==labelte(i,1)+1
+      correct=correct+1;
+    end
+    
+end
+Percentage(ctr,1)=correct/length(labelte)*100;
+ctr=ctr+1;
