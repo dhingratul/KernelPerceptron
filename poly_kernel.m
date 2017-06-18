25b9e8fb4440de06db42d3eb78722e535ed4be54
dhingratul <atul.dhingra@rutgers.edu>
2017-06-17 22:02:35 -0400

    Added comments

--- poly_kernel.m
+++ poly_kernel.m
@@ -1,6 +1,10 @@
+%% Polynomial Kernel
+% Input: x, y, p
+% Output: Dot Product
+% Note: Run from kperceptron.m
 function[dotproduct]=poly_kernel(x,y,p)
-    if nargin < 3
-	p = 2;
-    end
-    dotproduct=(1+(x*y'))^p;
+if nargin < 3
+    p = 2;
+end
+dotproduct=(1+(x*y'))^p;
 end
